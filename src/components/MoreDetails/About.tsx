8d1a65be1756b90ba9c9419a20f1ae8ea984dabc
Bittu Kumar <bittu@anamarhs.com>
2024-09-16 19:46:29 +0530

    improve about page

--- src/components/MoreDetails/About.tsx
+++ src/components/MoreDetails/About.tsx
@@ -2,20 +2,23 @@ import React from "react";
 import '@/app/globals.css';
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { Rating } from "@mui/material";
 
 const contributors = [
-    { name: "Krishna", img: "/contributors/krishna.jpg" },
-    { name: "Shivam", img: "/contributors/shivam.jpg" },
-    { name: "Raushan", img: "/contributors/raushan.jpg" },
-    { name: "Bittu", img: "/contributors/bittu.jpg" },
+    { name: "Krishna", role: "Frontend Developer", img: "/contributors/krishna.jpg", des: "A front-end expert with a passion for designing seamless user interfaces, Krishna specializes in building responsive websites and crafting pixel-perfect components." },
+    { name: "Shivam", role: "Backend Developer", img: "/contributors/shivam.jpg", des: "Shivam is a skilled back-end developer, ensuring our web solutions are fast, secure, and scalable by utilizing cutting-edge technologies and best practices." },
+    { name: "Raushan", role: "Full Stack Developer", img: "/contributors/raushan.jpg", des: "With a focus on full-stack development, Raushan bridges the gap between design and functionality, delivering high-performance web applications from start to finish." },
+    { name: "Bittu", role: "UI/UX Designer", img: "/contributors/bittu.jpg", des: "Bittu excels at UX/UI design, ensuring that our solutions are not only visually appealing but also user-friendly, optimizing user engagement and satisfaction." },
 ];
+
 
 export default function Contribute() {
     return (
-        <div className="flex flex-col items-center">
-           <Navbar/>
-            <div className="flex flex-col mt-20 mx-6 md:mx-24 items-center gap-20 text-white">
-                <div className="flex flex-col gap-5 text-center md:text-left w-2/3 max-w-7xl leading-10">
+        <div className="flex flex-col items-center bg-black">
+            <Navbar />
+            <div className="flex flex-col mt-10 mx-6 md:mx-24 items-center gap-16 text-white">
+                <div className="flex flex-col gap-7 items-center md:text-left w-2/3 max-w-7xl leading-10">
+                    <div className="bg-[#363733] px-5 rounded-full font-bold w-fit">About Us</div>
                     <h1 className="text-[#363733] text-center text-7xl"><span className="text-gray-400">About Anamarhs</span> <br /> Crafting Exceptional Websites</h1>
                     <p className="text-center text-xl leading-7 mt-10">Welcome to Anamarhs, your trusted software consultancy! Our dedicated team of professionals is committed to crafting beautiful and functional websites tailored precisely to meet your needs. Whether you're envisioning a sophisticated web application or a seamless digital solution, Anamarhs is here to turn your ideas into reality. Our innovative approach and user-friendly tools empower you to create cutting-edge applications effortlessly. Begin your journey with us today and transform your software visions into impactful digital experiences!</p>
                 </div>
@@ -71,11 +74,18 @@ export default function Contribute() {
                 </div>
                 <div className="flex flex-col gap-5 mb-10 mx-w-7xl">
                     <h1 className="text-6xl font-semibold text-gray-300 text-center">Contributors</h1>
-                    <div className="grid grid-cols-1 md:grid-cols-4 gap-10 mt-20 mb-20 max-w-7xl">
+                    <div className="grid grid-cols-1 md:grid-cols-4 gap-5 mt-20 mb-20 max-w-7xl">
                         {contributors.map((conrtibutor, index) => (
-                            <div key={index} className="flex flex-col items-center">
-                                <img src={conrtibutor.img} className=" rounded-xl  h-auto" />
-                                <h1 className="text-white text-center mt-5 text-2xl"> { conrtibutor.name } </h1>
+                            <div key={index} className="flex flex-col justify-between rounded-2xl p-10 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg border border-white border-opacity-20 shadow-lg">
+                                <Rating name="read-only" value={5} readOnly />
+                                <div className="py-5 text-white">{conrtibutor.des}</div>
+                                <div className="flex flex-wrap flex-row gap-5">
+                                    <img src={conrtibutor.img} className="rounded-full w-10 h-10" />
+                                    <div className="flex flex-col">
+                                        <h1 className="text-white text-xl">{conrtibutor.name}</h1>
+                                        <h3 className="underline">{conrtibutor.role}</h3>
+                                    </div>
+                                </div>
                             </div>
                         ))}
                     </div>
