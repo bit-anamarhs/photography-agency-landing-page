f30db9cfc109cf6803660c2b4d94b9dd657f15fe
Raushanraj8 <raushan@anamarhs.com>
2024-09-17 20:43:58 +0530

    build issue fixed

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -6,7 +6,7 @@ const ProductsPage = () => {
   return (
     <>
       <Navbar />
-      <div className="md:p-8  mt-32"> {/* Add margin-top here */}
+      <div className="md:p-8  mt-32"> 
 
         <h1 className="text-3xl font-bold mb-4 text-white text-center">Featured Products</h1>
         <FilterPage />
