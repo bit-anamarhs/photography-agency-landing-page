fb6e62bc74eb7693c10d4dcc9819747c81faf388
Bittu Kumar <bittu@anamarhs.com>
2024-09-17 18:11:23 +0530

    add footer in products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import FilterPage from "@/ProductsComponents/FilterPage";
 
@@ -5,10 +6,11 @@ const ProductsPage = () => {
   return (
     <>
       <Navbar />
-      <div className="p-8 pt-28"> {/* Add margin-top here */}
+      <div className="p-8 mt-32"> {/* Add margin-top here */}
         <h1 className="text-3xl font-bold mb-4 text-white text-center">Featured Products</h1>
         <FilterPage />
       </div>
+      <Footer />
     </>
   );
 };
